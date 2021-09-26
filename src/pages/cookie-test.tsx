06b4ce2517758b0ec4f773ce0e655479609245bc
ayataka0nk <naoto.kamba@gmail.com>
2021-09-26 20:59:42 +0900

    add fetch init options

--- src/pages/cookie-test.tsx
+++ src/pages/cookie-test.tsx
@@ -2,11 +2,16 @@ const CookieTest = () => {
   const login = async () => {
     await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/auth/login', {
       method: 'POST',
+      credentials: 'include',
     })
   }
   const confirm = async () => {
     const result = await fetch(
-      process.env.NEXT_PUBLIC_API_URL + '/api/auth/logged-in-user'
+      process.env.NEXT_PUBLIC_API_URL + '/api/auth/logged-in-user',
+      {
+        method: 'GET',
+        credentials: 'include',
+      }
     )
     console.log(result)
   }
